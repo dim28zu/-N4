0eb9d8752f9cdb53d71618ae780fdbb51ad8d79a
dim28zu <dim27zu@gmail.com>
2024-11-03 17:45:27 +0300

    Update _ya-tr-span data-index=_1-0_ data-translated=_true_ data-source-lang=_en_ data-target-lang=_ru_ data-value=_main.py

--- _ya-tr-span data-index=_1-0_ data-translated=_true_ data-source-lang=_en_ data-target-lang=_ru_ data-value=_main.py
+++ _ya-tr-span data-index=_1-0_ data-translated=_true_ data-source-lang=_en_ data-target-lang=_ru_ data-value=_main.py
@@ -1,10 +1,9 @@
+def add_numbers(a, b):
+    return a + b
 
-# Online Python - IDE, Editor, Compiler, Interpreter
-
-def sum(a, b):
-    return (a + b)
-
-a = int(input('Enter 1st number: '))
-b = int(input('Enter 2nd number: '))
-
-print(f'Sum of {a} and {b} is {sum(a, b)}')
+try:
+    a = float(input('Enter 1st number: '))
+    b = float(input('Enter 2nd number: '))
+    print(f'Sum of {a} and {b} is {add_numbers(a, b)}')
+except ValueError:
+    print("Please enter valid numbers.")
